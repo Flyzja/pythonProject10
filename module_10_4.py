23754a58dc702e83d363b18bbee1eef704a9332b
Flyzja <mamontova-75@mail.ru>
2024-10-15 14:25:36 +0300

    Очереди для обмена данными между потоками

--- module_10_4.py
+++ module_10_4.py
@@ -1,42 +1,67 @@
-from threading import Thread
-import gueue
-from time import sleep
+import threading
 import random
+from time import sleep
+from queue import Queue
 
-
-class Table:
+class Table:    #столы
 
     def __init__(self, number):
         self.number = number
-        self.guest  = None
+        self.guest = None
 
-class Cafe(gueue):
+class Guest(threading.Thread):                      # гость
+    def __init__(self, name: str):
+        super().__init__()
+        self.name = name
 
-    def __init__(self, queue):
-        self.queue = queue
-        self.tables = ()
+    def run(self):
+        sleep(random.randint(3, 10))
 
-    def guest_arrival(self, *guests):   #(прибытие гостей)
+class Cafe:
+    def __init__(self, *tables):
+        self.queue = Queue()
+        self.tables = list(tables)
+    def guest_arrival(self, *guests):     # прибытие гостей
         for guest in guests:
-             while True:
-                 self.tables = self.queue.put(guest)
+            table = self.find_free_table()
+            if table:
+                table.guest = guest
+                guest.start()
+                print(f'{guest.name} сел(-а) за стол номер {table.number}')
+            else:
+                self.queue.put(guest)
+                print(f'{guest.name} в очереди')
+    def discuss_guests(self):          # обслужить гостей
+        while not self.queue.empty() or self.any_guest_still_seated():
+            for table in self.tables:
+                if table.guest and not table.guest.is_alive():
+                    print(f'{table.guest.name} покушал(-а) и ушёл(ушла)')
+                    print(f'Стол номер {table.number} свободен')
+                    table.guest = None
+                if not self.queue.empty() and table.guest is None:
+                    next_guest = self.queue.get()
+                    table.guest = next_guest
+                    next_guest.start()
+                    print(f'{next_guest.name} вышел(-ла) из очереди и сел(-а) за стол номер {table.number}')
+    def find_free_table(self):
+        for table in self.tables:
+            if table.guest is None:
+                return table
+        return None
+    def any_guest_still_seated(self):
+        return any(table.guest is not None for table in self.tables)
 
-    def discuss_guests(self, *guests):   # обслужить гостей
-        guest =
-
-
-
-
-
-
-
-
-
-
-
-        self.guests = guests
-
-
-
-
-
+tables = [Table(number) for number in range(1, 6)]
+# Имена гостей
+guests_names = [
+'Maria', 'Oleg', 'Vakhtang', 'Sergey', 'Darya', 'Arman',
+'Vitoria', 'Nikita', 'Galina', 'Pavel', 'Ilya', 'Alexandra'
+]
+# Создание гостей
+guests = [Guest(name) for name in guests_names]
+# Заполнение кафе столами
+cafe = Cafe(*tables)
+# Приём гостей
+cafe.guest_arrival(*guests)
+# Обслуживание гостей
+cafe.discuss_guests()
